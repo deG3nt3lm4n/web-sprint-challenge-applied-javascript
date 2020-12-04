815905dc0cacee7734ec81e68e65643858487515
deG3nt3lm4n <th3g3nt3lm4n@gmail.com>
2020-12-04 14:22:30 -0500

    fin?

--- components/Cards.js
+++ components/Cards.js
@@ -7,16 +7,69 @@
 //
 // Write a function that takes a single article object and returns the following markup:
 //
-// <div class="card">
-//   <div class="headline">{Headline of article}</div>
-//   <div class="author">
-//     <div class="img-container">
-//       <img src={url of authors image} />
-//     </div>
-//     <span>By {author's name}</span>
-//   </div>
-// </div>
+{/* <div class="card">
+  <div class="headline">{Headline of article}</div>
+  <div class="author">
+    <div class="img-container">
+      <img src={url of authors image} />
+    </div>
+    <span>By {author's name}</span>
+  </div>
+</div> */}
 //
 // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
 //
 // Use your function to create a card for each of the articles, and append each card to the DOM.
+
+
+axios.get('https://lambda-times-api.herokuapp.com/articles')
+  .then(res => {
+    const d = res.data.articles
+    document.addEventListener('click', e => {
+      let bb = e.target.innerHTML;
+
+      for(const articles in d){
+
+        if(bb === articles){
+          const arrOfArticles = d[articles]
+          document.querySelector('.cards-container').innerHTML = ''
+          createArticle(arrOfArticles)
+        }
+      }
+    })
+
+  })
+  .catch(err => {
+    console.log(err)
+  })
+
+function createArticle(arr){
+
+
+  let html = '';
+
+  arr.forEach(arr => {
+    // console.log(arr, 'single card', typeof arr)
+    html +=  `
+    <div class="card">
+      <div class="headline">${arr.headline}</div>
+        <div class="author">
+          <div class="img-container">
+          <img src=${arr.authorPhoto} />
+        </div>
+        <span>By ${arr.authorName}</span>
+      </div>
+    </div>
+    `
+
+  })
+
+
+  document.querySelector('.cards-container').innerHTML += html;
+  document.querySelector('.card').addEventListener('click', e => {
+    // console.log(e.target.children[0].innerText)
+
+    console.log(e.target.parentNode.children[0].innerText)
+  })
+}
+
